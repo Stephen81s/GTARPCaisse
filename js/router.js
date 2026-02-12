73f0710d71daee098c8040df819c5e11beb0b23f
Stephen81s <soupart.stephen@gmail.com>
2026-02-12 19:20:52 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,12 +1,17 @@
-// ROUTEUR SIMPLE — charge les pages dans #page-container
-
 async function loadPage(page) {
     const container = document.getElementById("page-container");
 
     try {
         const res = await fetch(`interfaces/${page}.html`);
+
+        if (!res.ok) {
+            container.innerHTML = "<h2>Page introuvable</h2>";
+            return;
+        }
+
         const html = await res.text();
         container.innerHTML = html;
+
     } catch (err) {
         container.innerHTML = "<h2>Erreur de chargement</h2>";
     }
